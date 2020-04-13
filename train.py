007546e90d4c9cb70848d7db3588ba6cced586ab
Michael Patel <michael.patel94@gmail.com>
2020-04-13 19:36:58 -0400

    Adding D and G loss fns

--- train.py
+++ train.py
@@ -24,6 +24,37 @@ from model import Discriminator, Generator
 
 
 ################################################################################
+# discriminaor loss function
+def discriminator_loss(real_output, fake_output):
+    cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+
+    real_loss = cross_entropy(
+        tf.ones_like(real_output),
+        real_output
+    )
+
+    fake_loss = cross_entropy(
+        tf.zeros_like(fake_output),
+        fake_output
+    )
+
+    total_loss = real_loss + fake_loss
+    return total_loss
+
+
+# generator loss function
+def generator_loss(generated_output):
+    cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+
+    generated_loss = cross_entropy(
+        tf.ones_like(generated_output),
+        generated_output
+    )
+
+    return generated_loss
+
+
+################################################################################
 # Main
 if __name__ == "__main__":
     # print TF version
